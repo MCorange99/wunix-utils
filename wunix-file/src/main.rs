fccac122c448cbed344615a8b751bda53061af82
MCorange99 <gvidasjuknevicius2@gmail.com>
2024-03-07 18:45:00 +0200

    wunix-file

--- wunix-file/src/main.rs
+++ wunix-file/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use file_format::FileFormat;
+
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// File to inspect
+    file: String
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let f_fmt = FileFormat::from_file(&args.file)?;
+
+    println!("{}: {} {} {}", args.file, f_fmt.media_type(),  f_fmt.short_name().unwrap_or(""),  f_fmt.name());
+
+
+
+    Ok(())
 }
